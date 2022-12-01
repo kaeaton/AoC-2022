e4c2c4b8810082e4962f941558286d33a8b0d899
Katrina Eaton <katrina.eaton@gmail.com>
2022-12-01 10:32:46 -0800

    AoC - Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,40 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxCalories = 0
+        var currentCalories = 0
+        val input = readInput("input_1a")
+        input.forEach {
+            if(it.isNotEmpty()) {
+                currentCalories += it.toInt()
+            } else {
+                if(currentCalories > maxCalories) {
+                    maxCalories = currentCalories
+                }
+                currentCalories = 0
+            }
+        }
+        return maxCalories
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var maxCalories = ArrayList<Int>()
+        var currentCalories = 0
+        val input = readInput("input_1a")
+        input.forEach {
+            if(it.isNotEmpty()) {
+                currentCalories += it.toInt()
+            } else {
+                maxCalories.add(currentCalories)
+                currentCalories = 0
+            }
+        }
+
+        maxCalories.sortDescending()
+
+        return maxCalories.slice(0..2).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("input_1a")
     println(part1(input))
     println(part2(input))
 }

3767b2e727e951c69f874d0feba7107fe5f838a3
Katrina Eaton <katrina.eaton@ucsf.edu>
2022-12-02 09:15:20 -0800

    Day2 pre testing

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,6 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+	
+		var score = 0
+		
+		// val A = "Rock"
+		// val B = "Paper"
+		// val C = "Scissors"
+		
+		// Rock = X for 1 point
+		// Paper = Y for 2 points
+		// Scissors = Z for 3 points
+		val responseValues = mapOf("X" to 1, 
+								   "Y" to 2, 
+								   "Z" to 3)
+								   
+		val winningCombos = mapOf("A Y" to 8, 
+								  "B Z" to 9, 
+								  "C X" to 7,
+								  "A X" to 4,
+								  "B Y" to 5,
+								  "C Z" to 6)
+		
+		input.forEach {
+			if(winningCombos.containsKey(it) == true) {
+				score += winningCombos[it]
+			} else {
+				val elementPlayed = it.split(" ")[1]
+				score += responseValues[elementPlayed]
+			}
+		}
+	
+        return score
     }
 
     fun part2(input: List<String>): Int {
